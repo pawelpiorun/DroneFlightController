17d5cd80d98762a13869758c89e8af3d79fb1caf
piorun <dev.pawelpiorun@gmail.com>
2019-12-10 20:33:48 +0100

    Succesful motor startup with ESC. Comments for ESCTest.

--- Testing/ESCTest/ESCTest.ino
+++ Testing/ESCTest/ESCTest.ino
@@ -1,9 +1,29 @@
+// IMPORTANT - READ THIS!
+/*
+ * - Disconnect the battery before starting this program.
+ * - Initial pulse for ESC is 2000us to show maximum range value!!!
+ * - After 3 beeps "ti da di" - ESC is initialized. 
+ * Number of beeps may depend on battery voltage (in this case it's 3S).
+ * - 1 short beep states that upper range value has been detected
+ * - YOU HAVE 2 SECONDS TO WRITE "1" TO SERIAL PORT - it will change pulse to 1000us,
+ * in order to show the minimum value to ESC.
+ * - 1 short beep will state that lower range value has been detected.
+ * - 1 long beep will be a message "Ready to fly"
+ * - entering
+ * 
+ * Read SimonK ESC startup procedure for more info.
+ * It is possible to initialize esc pulse with 1000us, but the upper value will not be detected.
+ * SimonK ESC firmware should be working fine even without this procedure though.
+ * As default is should have 1000us to 2000us range.
+ */
+
 #define escPin1 3
 
+// TODO: Check for 4000 us
 const float refreshPeriod = 20000; //us
-long refreshStartTime = 0;
+long nextRefreshTime = 0;
 
-float escPulse1 = 1000; //us
+float escPulse1 = 2000; //us
 
 void setup() {
   pinMode(escPin1, OUTPUT);
@@ -11,11 +31,22 @@ void setup() {
 }
 
 void loop() {
-  while (micros() < refreshStartTime) { };
-  refreshStartTime = micros() + refreshPeriod;
+
+  // wait until we hit next refresh time
+  while (micros() < nextRefreshTime) { };
+
+  // set next refresh time
+  nextRefreshTime = micros() + refreshPeriod;
+
+  // write PWM to esc
   digitalWrite(escPin1, HIGH);
   delayMicroseconds(escPulse1);
   digitalWrite(escPin1, LOW);
+
+  // get command to change pulse
+  // 1 - 1000 (no speed)
+  // 2 - 1200
+  // 3 - 1500
   if (Serial.available() > 0)
   {
     char input = Serial.read();
@@ -35,4 +66,7 @@ void loop() {
       Serial.println("Pulse changed to 1500");
     }
   }  
+
+  // print current pulse
+  Serial.println(escPulse1);
 }
